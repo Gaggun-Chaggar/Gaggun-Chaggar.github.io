6ce38fdba77fd1ab256b3f727dd796a634701317
Gaggun Chaggar <gaggunchaggar@gmail.com>
2023-07-01 09:42:15 +0100

    replace tab view change with css

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -34,11 +34,6 @@ const handleTabs = (tablist) => {
   });
 
   setTabActive(0);
-
-  const { switchView } = useSwitchView(tablist, tabs, tabPanels);
-
-  switchView();
-  window.addEventListener("resize", switchView);
 };
 
 /**
@@ -176,72 +171,6 @@ const useKeyboardEvents = (tabs) => {
 
   return {
     createOnKeydownEvent,
-  };
-};
-
-/**
- *
- * @param {HTMLElement} tablist
- * @param {NodeListOf<Element>} tabs
- * @param {NodeListOf<Element>} tabPanels
- */
-const useSwitchView = (tablist, tabs, tabPanels) => {
-  const tablistParent = tablist.parentElement;
-  let view = "desktop";
-  const convertTabpanelsToMobileView = () => {
-    tablistParent.removeChild(tablist);
-    tabPanels.forEach((tabpanel, index) => {
-      const tab = tabs[index];
-      const tabTitle = tab.textContent;
-
-      const h2 = document.createElement("h2");
-      h2.innerText = tabTitle;
-      h2.className = "tab-heading fg-secondary p-2";
-
-      if (index === 0) {
-        h2.className += " sr-only";
-      }
-
-      tabpanel.prepend(h2);
-
-      tabpanel.removeAttribute("role");
-      tabpanel.classList.remove("tabpanel");
-      tabpanel.classList.add("section");
-
-      view = "mobile";
-    });
-  };
-
-  const convertTabpanelsToDesktopView = () => {
-    tablistParent.appendChild(tablist);
-
-    tabPanels.forEach((tabpanel) => {
-      const tabHeading = tabpanel.querySelector(".tab-heading");
-      tabpanel.setAttribute("role", "tabpanel");
-      tabpanel.classList.add("tabpanel");
-      tabpanel.classList.remove("section");
-
-      tabpanel.removeChild(tabHeading);
-    });
-    view = "desktop";
-  };
-
-  const switchView = () => {
-    const width = document.querySelector("body").offsetWidth;
-    const shouldSwitchToMobileView = width < 768 && view === "desktop";
-    const shouldSwitchToDesktopView = width >= 768 && view === "mobile";
-
-    if (shouldSwitchToMobileView) {
-      return convertTabpanelsToMobileView();
-    }
-
-    if (shouldSwitchToDesktopView) {
-      convertTabpanelsToDesktopView();
-    }
-  };
-
-  return {
-    switchView,
   };
 };
 
