3ab4b6c83af03b6ad3a42893232cc791e7d7b577
Gaggun Chaggar <gaggunchaggar@gmail.com>
2023-07-03 22:25:42 +0100

    shrink header when scrolling on mobile

---
+++ src/static/js/header-animation.js
@@ -0,0 +1,21 @@
+const header = document.querySelector("header");
+
+let lastScrollValue = 0;
+document.addEventListener("scroll", () => {
+  const down = window.scrollY > lastScrollValue;
+
+  const up = window.scrollY < lastScrollValue;
+
+  lastScrollValue = window.scrollY;
+  console.log(up, down);
+
+  if (up && window.scrollY < 200) {
+    header.classList.remove("shrink");
+    return;
+  }
+
+  if (down && window.scrollY >= 200) {
+    header.classList.add("shrink");
+    return;
+  }
+});
