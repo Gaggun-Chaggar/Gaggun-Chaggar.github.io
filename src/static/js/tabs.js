97aecbdab25f3be0851122dee4c68faf6e4b7e83
Gaggun Chaggar <gaggunchaggar@gmail.com>
2023-07-10 14:47:26 +0100

    delay scroll up on tab change

--- src/static/js/tabs.js
+++ src/static/js/tabs.js
@@ -29,7 +29,7 @@ const useSetTabState = (tabs, tabPanels, tabTracker) => {
     setTimeout(() => {
       tabPanels[index].classList.remove("hidden-fade-in");
       window.scrollTo({ top: 0 });
-    }, 1);
+    }, 100);
 
     activeTabIndex = index;
 
