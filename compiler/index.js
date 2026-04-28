f0385765df02cfcd58b134e0cab577052b20d909
Gaggun-Chaggar <gaggunchaggar@gmail.com>
2026-04-28 23:24:08 +0100

    remove blog sections, simplify tags

--- compiler/index.js
+++ compiler/index.js
@@ -1,6 +1,5 @@
-import { processBlog } from "./processor/blog.js";
 import { processCSS } from "./processor/css.js";
 import { processPages } from "./processor/pages.js";
 
-await Promise.all([processPages(), processBlog()]);
+await processPages();
 processCSS();
